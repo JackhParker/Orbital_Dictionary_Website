340bdb9b1e975fa7ede29d7a7cee17ca37602a52
Jack Parker <jackhparker98@gmail.com>
2022-02-10 11:39:00 -0500

    saving before pull

--- assets/js.js
+++ assets/js.js
@@ -1,6 +1,6 @@
 
 let clientID = "OMagC9GezDzZQKGjkT6QHaqQX74W3xukdyNYzqgP";
-let endpoint = `https://api.nasa.gov/planetary/apod?api_key=${clientID}&count=1`;
+let endpoint = `https://api.nasa.gov/planetary/apod?api_key=${clientID}`;
 var searchBtn = document.querySelector("#search")
 let searchInput = document.getElementById("searchinput")
 let imageElement = document.querySelector("#randomImage");
@@ -8,17 +8,29 @@ let imageLink = document.querySelector("imageLink");
 
 
 // new global var being adding fo the facts button
-let randomFactsButton = document.querySelector(`#randomFactsButton`);
+let randomFactsButton = document.getElementById(`randomFactsButton`);
 
+var factsArray = [`mars`, `earth`, `moon`, `jupiter`, `titan`, `io`, `neptune`, `venus`, `saturn`];
+
+// original fetch code for image
+// fetch(endpoint)
+//     .then(function (response) {
+//         return response.json();
+// })
+// // .then(function (data) {
+// //     for (let i = 0; i < data.length; i++) {
+// //         imageElement.src = data[i].url;
+// // }
+// // })
 
 fetch(endpoint)
     .then(function (response) {
         return response.json();
 })
 .then(function (data) {
-    for (let i = 0; i < data.length; i++) {
-        imageElement.src = data[i].url;
-}
+    // for (let i = 0; i < data.length; i++) {
+        imageElement.src = data.url;
+// }
 })
 
 function navFn (){
@@ -37,10 +49,10 @@ searchBtn.addEventListener("click", function(event){
     localStorage.setItem("recent", searchInput.value);
 })
 
-// new code thats for the random fact function and the event listener for it
+// new code thats for the random fact function and the event listener for it by JACK P
 function randomFacts(){
-    var factCard = document.getElementById("#fact-card")
-    var factInfo = "https://api.le-systeme-solaire.net/rest/bodies/mars"
+    var factCard = document.getElementById("fact-card")
+    var factInfo = `https://api.le-systeme-solaire.net/rest/bodies/moon`
 
     fetch(factInfo)
     .then(function(responce){
@@ -48,13 +60,27 @@ function randomFacts(){
         
     })
     .then(function(data){
+        var name = document.createElement(`h1`);
+        var facts2 = document.createElement(`h2`);
         var facts = document.createElement(`p`);
-        facts.textContent = data;
+        var facts3 = document.createElement(`p`);
+
+        name.textContent = data.englishName;
+        facts2.textContent = data.bodyType;
+        facts.textContent = `Mass in Kg: ` + data.mass.massValue + ` x 10^` + data.mass.massExponent + ` Kg`;
+        facts3.textContent = `Gravity: ` + data.gravity + ` Meters/Sec`;
+
+        factCard.appendChild(name);
+        factCard.appendChild(facts2);
+        factCard.appendChild(facts);
+        factCard.appendChild(facts3);
         console.log(data);
     })
-    
+    // for (var i=0; i < factsArray.length; i++) {
+//   
+//}
 }
-
+// FUNCTION FOR THE
 randomFactsButton.addEventListener(`click`, function(event){
     event.preventDefault();
     randomFacts();
